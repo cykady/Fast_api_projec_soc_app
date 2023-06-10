db0fd42c5b32f429456aa7d17e06c1a04b10beea
cykady <cykadyy@gmail.com>
2023-06-10 17:09:20 +0200

    another try

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,17 +1,6 @@
 from app import schemas
 import pytest
 
-#ALL POSTS TESTS
-# def test_get_all_posts(authorized_client, test_posts):
-#     res = authorized_client.get("/posts/")
-
-#     def validate(post):
-#         return schemas.PostOut(**post)
-#     posts_map = map(validate, res.json())
-#     posts_list = list(posts_map)
-
-#     assert len(res.json()) == len(test_posts)
-#     assert res.status_code == 200
 
 def test_unauthorized_get_all_posts(client,test_posts):
     res = client.get(f"/posts/{test_posts[0].id}")
